c56e48e552f4443e10cefa0c466a624550d9af80
nunthanok <nunthanok41@gmail.com>
2021-03-11 21:59:24 +0700

    Second Commit

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <!-- Navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container">
-                <a class="navbar-brand" href="index.php">Football Club</a>
+                <a class="navbar-brand" href="index.php">Football Club!!!</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
